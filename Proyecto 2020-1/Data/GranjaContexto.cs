707ad3995510a54696104bbdd4d52614418a7dfd
Alvaro <Alvaro@DESKTOP-V2PKLEK>
2020-03-08 14:44:59 -0400

    proyecto

---
+++ Proyecto 2020-1/Data/GranjaContexto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Proyecto_2020_1.Models;
+
+namespace Proyecto_2020_1.Data
+{
+    public class GranjaContexto: DbContext
+    {
+        public DbSet<Granja> granjas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=DataBase/Data.db");
+        }
+    }
+}
---
+++ Proyecto 2020-1/Migrations/20200308183843_inicial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Proyecto_2020_1.Migrations
+{
+    public partial class inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "granjas",
+                columns: table => new
+                {
+                    MadreId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Raza = table.Column<string>(nullable: true),
+                    Corral = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_granjas", x => x.MadreId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "granjas");
+        }
+    }
+}
